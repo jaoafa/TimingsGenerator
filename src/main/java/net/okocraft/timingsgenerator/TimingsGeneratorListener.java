f52b2e239a2f9d4c2293ff01e65f3b9c0b167ad6
Siroshun09 <siro.shun8@gmail.com>
2020-06-25 01:00:39 +0900

    [Add] config インスタンス

--- src/main/java/net/okocraft/timingsgenerator/TimingsGeneratorListener.java
+++ src/main/java/net/okocraft/timingsgenerator/TimingsGeneratorListener.java
@@ -1,16 +1,20 @@
 package net.okocraft.timingsgenerator;
 
 import co.aikar.timings.TimingsReportListener;
+import com.github.siroshun09.configapi.bukkit.BukkitConfig;
+import com.github.siroshun09.configapi.common.Configuration;
 import com.github.siroshun09.filelogger.FileLogger;
 import org.bukkit.ChatColor;
 
 public class TimingsGeneratorListener extends TimingsReportListener {
 
+    private final Configuration config;
     private final FileLogger fileLogger;
 
     TimingsGeneratorListener(TimingsGeneratorPlugin plugin) {
         super(plugin.getServer().getConsoleSender());
 
+        config = new BukkitConfig(plugin, "config.yml", true);
         fileLogger = new FileLogger(plugin.getDataFolder().toPath().resolve("logs"));
     }
 
